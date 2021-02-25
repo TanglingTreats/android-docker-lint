fbe5e0e785cd277a115605926e4187dff1e2acd3
Edwin <edwintang10@gmail.com>
2021-02-26 01:17:37 +0800

    Change output command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
 lint_path=/github/workspace/app/build/reports/lint-results.html
-echo $lint_path > lint-results.html
+cat $lint_path > lint-results.html
 chmod 755 ./lint-results.html
