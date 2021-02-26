9c7422bcb0f9e48c1d44cf2590d496414f4b8f0e
Edwin Tang <edwintang10@gmail.com>
2021-02-26 01:52:15 +0000

    Change commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ time=$(date)
 echo "::set-output name=time::$time"
 lint_path=/github/workspace/app/build/reports/lint-results.html
 cat $lint_path > lint-results.html
-chmod 755 ./lint-results.html
-chown -R ./lint-results.html
+sh -c "sudo chmod -R 755 ./lint-results.html"
+sh -c "chown -R ./lint-results.html"
