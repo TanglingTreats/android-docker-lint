46df0aa9f8eb0fc532cc4e81157278e9509615f8
Edwin Tang <edwintang10@gmail.com>
2021-02-26 01:42:14 +0000

    Remove sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ time=$(date)
 echo "::set-output name=time::$time"
 lint_path=/github/workspace/app/build/reports/lint-results.html
 cat $lint_path > lint-results.html
-sudo chmod 755 ./lint-results.html
-sudo chown -R ./lint-results.html
+chmod 755 ./lint-results.html
+chown -R ./lint-results.html
