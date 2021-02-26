51a687a57192d610805d677fb086a0e4347a1f62
Edwin <edwintang10@gmail.com>
2021-02-26 14:07:16 +0800

    Comment chown line

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ time=$(date)
 echo "::set-output name=time::$time"
 lint_path=/github/workspace/app/build/reports/lint-results.html
 cat $lint_path > lint-results.html
-sh -c "chmod -R 755 lint-results.html"
-sh -c "chown -R $USER lint-results.html"
+sh -c "chmod -R 755 ./lint-results.html"
+#sh -c "chown -R $USER ./lint-results.html"
